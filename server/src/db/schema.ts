6065f3cbe66b9a99d92b832947802d0ce083643f
Mikael Sirqueira <silmike14@yahoo.com.br>
2024-09-11 18:51:01 -0300

    feat: add mock data

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -2,7 +2,9 @@ import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
 
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
@@ -14,7 +16,7 @@ export const goalCompletions = pgTable('goal_completions', {
   id: text('id')
     .primaryKey()
     .$defaultFn(() => createId()),
-  goadId: text('goal_id')
+  goalId: text('goal_id')
     .references(() => goals.id)
     .notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
